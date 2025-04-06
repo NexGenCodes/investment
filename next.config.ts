eeb421d1066e1470f2e46b9e8d1e1d001b2eee8a
emmanuel <74940049+NexGenCodes@users.noreply.github.com>
2025-04-06 21:11:09 +0200

    fixed middleware issue

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,13 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.ejs$/,
+      use: "raw-loader",
+    });
+    return config;
+  },
+};
 
 export default nextConfig;
